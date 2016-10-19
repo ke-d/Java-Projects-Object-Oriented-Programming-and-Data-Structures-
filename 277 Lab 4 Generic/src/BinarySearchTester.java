446cdb0426e59d2d3b0981cf7162fcd4bb65d958
mrdokenny <mrdokenny@gmail.com>
2016-10-19 10:43:46 -0700

    Finalized Lab 4

--- 277 Lab 4 Generic/src/BinarySearchTester.java
+++ 277 Lab 4 Generic/src/BinarySearchTester.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BinarySearchTester {
 
 	public static void main(String[] args) {
+		boolean isSorted = false;
 		Faculty[] fac = {new Faculty("Bob", 2021),new Faculty("Carl", 1900),new Faculty("Tom", 3421),new Faculty("Karl", 2919),new Faculty("Dezel", 1000)};
 		BinarySearcher<Faculty> bs = new BinarySearcher<>(fac);
 		Scanner in = new Scanner(System.in);
@@ -15,9 +16,18 @@ public class BinarySearchTester {
 			case 2:
 				InsertionSort.sort(fac);
 				System.out.println("List Sorted");
+				isSorted = true;
 				break;
 			case 3:
-				search(bs, in);
+				if (isSorted) {
+					try {
+						System.out.println(fac[search(bs, in)].getName() + " found!");
+					} catch (ArrayIndexOutOfBoundsException e) {
+						System.out.println("Id Num not found");
+					}
+				} else {
+					System.out.println("List not sorted");
+				}
 				break;
 			}
 			System.out.println();
@@ -26,12 +36,23 @@ public class BinarySearchTester {
 		in.close();
 	}
 
-	private static void search(BinarySearcher<Faculty> bs, Scanner in) {
+	/**
+	 * Searches for the an id num
+	 * @param bs the searcher
+	 * @param in the scanner
+	 * @return the index where the id num exists
+	 */
+	private static int search(BinarySearcher<Faculty> bs, Scanner in) {
 		System.out.println("Search for what ID number?");
 		int input = in.nextInt();
-		System.out.println("Search for Id " + input + ": " + bs.search(new Faculty("", input)));
+		return bs.search(new Faculty("", input));
 	}
 
+	/**
+	 * Display a menu
+	 * @param in the scanner
+	 * @return the menu index
+	 */
 	private static int menu(Scanner in) {
 		System.out.println("1. Display List");
 		System.out.println("2. Sort List");
@@ -41,6 +62,10 @@ public class BinarySearchTester {
 		return input;
 	}
 
+	/**
+	 * Print the array
+	 * @param fac the array to print
+	 */
 	private static void printArray(Faculty[] fac) {
 		for(int i = 0; i < fac.length; i++) {
 			System.out.println(fac[i].toString());
