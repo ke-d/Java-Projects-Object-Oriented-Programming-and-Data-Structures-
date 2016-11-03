f102e6a8093c833b6fdeb9c2d697fe9efba5a5d2
mrdokenny <mrdokenny@gmail.com>
2016-11-02 17:31:29 -0700

    Added Methods for Lab 6
    -Add, Remove, Modify, Display Methods

--- 277 Lab 6 Collections 2/src/Employee.java
+++ 277 Lab 6 Collections 2/src/Employee.java
@@ -1,5 +1,5 @@
 
-public class Employee {
+public class Employee implements Comparable <Employee>{
 	private int id;
 	private String lastName;
 	private String firstName;
@@ -50,6 +50,65 @@ public class Employee {
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	@Override
+	public int compareTo(Employee arg0) {
+		if(lastName.equals(arg0.getLastName())) {
+			if(firstName.equals(arg0.getFirstName())) {
+				return id - arg0.getId();
+			}
+			return firstName.compareTo(arg0.getFirstName());
+		}
+		return lastName.compareTo(arg0.getLastName());
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", firstName=" + firstName + "]";
 	}
 	 
 
--- 277 Lab 6 Collections 2/src/MapTester.java
+++ 277 Lab 6 Collections 2/src/MapTester.java
@@ -4,41 +4,113 @@ import java.util.TreeMap;
 
 public class MapTester {
 	public enum Menu {
-		ADD(1), REMOVE(2), MODIFIY(3), PRINT(4);
-		
-		private int code;
-		 
-	    private Menu(int code) {
-	        this.code = code;
-	    }
-	 
-	    public int getCode() {
-	        return code;
-	    }
+		ADD, REMOVE, MODIFY, PRINT, EXIT;
 	}
+	
+	public enum Performance {
+		EXCELLENT, GOOD, SATISFACTORY, IMPROVEMENT;
+	}
+	
 	static Scanner input;
 	public static void main(String[] args) {
 		input = new Scanner (System.in);
-		Map<Integer, Employee> e = new TreeMap<Integer, Employee> ();
+		Map<Integer, Employee> employeeMap = new TreeMap<Integer, Employee> ();
 		
-		Map<Employee, String> ep = new TreeMap<Employee, String> ();
-		
+		Map<Employee, Performance> performanceMap = new TreeMap<Employee, Performance> ();
+
 		int in = printMenuAndGetChoice();
-		while(in >= 1 || in <= 4) {
-			switch (Menu) {
-			
+		while(in >= 1 && in <= 4) {
+		in--;
+		Menu choice = Menu.values()[in];
+			switch (choice) {
+			case ADD:
+				addEmployee(employeeMap, performanceMap);
+				break;
+			case REMOVE:
+				removeEmployee(employeeMap, performanceMap);
+				break;
+			case MODIFY:
+				modifyEmployee(employeeMap, performanceMap);
+				break;
+			case PRINT:
+				printEmployee(performanceMap);
+				break;
+			case EXIT:
+				break;
 			}
+			System.out.println();
+			in = printMenuAndGetChoice();
 		}
 
 	}
 	
+	private static void addEmployee(Map<Integer, Employee> employeeMap, Map<Employee, Performance> performanceMap) {
+		int id;
+		String lastName;
+		String firstName;
+		
+		System.out.println("Enter ID:");
+		id = input.nextInt();
+		System.out.println("Enter First Name:");
+		firstName = input.next();
+		System.out.println("Enter Last Name:");
+		lastName = input.next();
+		
+		Employee em = new Employee(id, lastName, firstName);
+		employeeMap.put(id, em);
+		Performance perf = getPerformanceFromUser();
+		performanceMap.put(em, perf);
+	}
+
+
+	private static void removeEmployee(Map<Integer, Employee> employeeMap, Map<Employee, Performance> performanceMap) {
+		System.out.println("Enter an ID to remove:");
+		int id = input.nextInt();
+		if(employeeMap.containsKey(id)) {
+			Employee removed = employeeMap.get(id);
+			performanceMap.remove(removed);
+			employeeMap.remove(id);	
+			System.out.println("Employee Removed");
+		} else {
+			System.out.println("Employee not in Map");
+		}
+		
+	}
+	
+	private static void modifyEmployee(Map<Integer, Employee> employeeMap, Map<Employee, Performance> performanceMap) {
+		System.out.println("Enter an ID to remove:");
+		int id = input.nextInt();
+		if(employeeMap.containsKey(id)) {
+			Employee modify = employeeMap.get(id);
+			Performance perfModify = getPerformanceFromUser();
+			performanceMap.put(modify, perfModify);
+		} else {
+			System.out.println("Employee not in Map");
+		}
+	}
+
+	private static void printEmployee(Map<Employee, Performance> performanceMap) {
+		System.out.println(performanceMap.toString());
+		
+	}
+
 	public static int printMenuAndGetChoice() {
-		System.out.println("1. Add");
-		System.out.println("2. Remove");
-		System.out.println("3. Modify");
-		System.out.println("4. Print");
+		Menu items[] = Menu.values();
+		for(int i = 0; i < items.length; i++) {
+			System.out.println((i + 1) + ". " + items[i]);
+		}
 		int in = input.nextInt();
 		return in;
 	}
 
+	private static Performance getPerformanceFromUser() {
+		System.out.println("Enter a performance:");
+		Performance items[] = Performance.values();
+		for(int i = 0; i < items.length; i++) {
+			System.out.println((i + 1) + ". " + items[i]);
+		}
+		int in = input.nextInt();
+		in--;
+		return items[in];
+	}
 }
